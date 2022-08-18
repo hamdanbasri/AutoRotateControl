d73c2d83644f4380df4034c732a987cdbb2bf25b
Hamdan Basri <38688313+hamdanbasri@users.noreply.github.com>
2022-08-19 01:02:54 +0800

    Initial Commit

---
+++ AutoRotateControl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotateControl : MonoBehaviour
+{
+
+    public bool PosX, NegX, PosY, NegY, PosZ, NegZ;
+
+    public float speed = 10f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (PosX == true)
+        {
+            transform.Rotate(Vector3.right, speed * Time.deltaTime);
+        }
+
+        if (NegX == true)
+        {
+            transform.Rotate(Vector3.left, speed * Time.deltaTime);
+        }
+
+        if (PosY == true)
+        {
+            transform.Rotate(Vector3.up, speed * Time.deltaTime);
+        }
+
+        if (NegY == true)
+        {
+            transform.Rotate(Vector3.down, speed * Time.deltaTime);
+        }
+
+        if (PosZ == true)
+        {
+            transform.Rotate(Vector3.back, speed * Time.deltaTime);
+        }
+
+        if (NegZ == true)
+        {
+            transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+        }        
+    }
+}
